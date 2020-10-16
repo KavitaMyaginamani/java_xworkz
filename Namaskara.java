3dc58955dae37ea5358c9aa6d9c3e6261bb65e51
KavitaMyaginamani <72960895+KavitaMyaginamani@users.noreply.github.com>
2020-10-16 11:06:58 +0530

    first commit

---
+++ Namaskara.java
@@ -0,0 +1,23 @@
+class Wish
+{
+public static void main(String[] kavita)
+{
+System.out.println("Namaskara");
+}
+}
+
+class Camera
+{
+public static void main(String[] Camera)
+{
+System.out.println("Camera is a kodak");
+System.out.println("price is lot");
+}
+}
+class Whether
+{
+public static void main(String[] kavita)
+{
+System.out.println("city");
+}
+}
