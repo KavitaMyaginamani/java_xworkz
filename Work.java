f60988fffe3b3299cf2878ee4f209c98763b04ff
KavitaMyaginamani <72960895+KavitaMyaginamani@users.noreply.github.com>
2020-10-18 11:11:09 +0530

    fourth prgm

---
+++ Work.java
@@ -0,0 +1,17 @@
+class Worksname
+{
+public static void main(String[] dgvj)
+{
+System.out.println("kavita");
+music();
+}
+public static void music()
+{
+System.out.println(" liesning a lyrics");
+drinking();
+}
+public static void drinking()
+{
+System.out.println(" drinking Tea");
+}
+}
