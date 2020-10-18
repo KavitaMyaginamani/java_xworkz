8ea98ed890c479ba14385a2e1a6c6296abd5df20
KavitaMyaginamani <72960895+KavitaMyaginamani@users.noreply.github.com>
2020-10-18 11:05:30 +0530

    Third prgm

---
+++ World.java
@@ -0,0 +1,14 @@
+class Worldtravel
+{
+public static void main(String[] asdx)
+{
+System.out.println("journey");
+write();
+write();
+write();
+}
+public static void write()
+{
+System.out.println("Novel");
+}
+}
