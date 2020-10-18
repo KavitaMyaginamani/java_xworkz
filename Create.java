18bcdcac2f14c77a018d205dfd44a3701d26baa7
KavitaMyaginamani <72960895+KavitaMyaginamani@users.noreply.github.com>
2020-10-18 11:14:17 +0530

    fifth prgm

---
+++ Create.java
@@ -0,0 +1,27 @@
+class Draw
+{
+public static void main(String[] vbnc)
+{
+System.out.println("painting");
+washing();
+}
+public static void washing()
+{
+System.out.println("Cloths");
+eating();
+}
+public static void eating()
+{
+System.out.println("Fruits");
+playing();
+}
+public static void playing()
+{
+System.out.println("chess");
+festival();
+}
+public static void festival()
+{
+System.out.println("Celebration");
+}
+}
