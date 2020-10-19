51a770bd1e6fc7357f36f86f0671cc6bd9e96e26
KavitaMyaginamani <72960895+KavitaMyaginamani@users.noreply.github.com>
2020-10-19 23:11:22 +0530

    eighth prgm

---
+++ Mobile.java
@@ -0,0 +1,35 @@
+public class Mobile
+{
+public static void main(String[] asdf)
+{
+	//data-type var-name;
+System.out.println("vivo");
+short priceMoney;
+priceMoney=15000;
+System.out.println(priceMoney);
+byte numberOfHoursPerDay;
+numberOfHoursPerDay=24;
+System.out.println(numberOfHoursPerDay);
+int priceOfGold;
+priceOfGold=50000;
+System.out.println(priceOfGold);
+long populationOfAmerica;
+populationOfAmerica=331002651;
+System.out.println(populationOfAmerica);
+float percentageOfChargeInMobile;
+percentageOfChargeInMobile=99;
+System.out.println(percentageOfChargeInMobile);
+double chargeOfElectron;
+chargeOfElectron=1.9e-19;
+System.out.println(chargeOfElectron);
+boolean isTodayMonday;
+isTodayMonday=false;
+System.out.println(isTodayMonday);
+char asciiValue;
+asciiValue='B';
+String wish;
+wish="hello";
+System.out.println(wish);
+
+}
+}
