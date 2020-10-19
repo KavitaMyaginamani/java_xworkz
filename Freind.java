901e1f2b228ccbd9b55aa95713a2ad30a6510e07
KavitaMyaginamani <72960895+KavitaMyaginamani@users.noreply.github.com>
2020-10-19 23:05:54 +0530

    seventh prgm

---
+++ Freind.java
@@ -0,0 +1,34 @@
+public class Freind
+{
+public static void main(String[] freinds)
+{
+	//data-type var-name;
+System.out.println("bestie");
+short priceOfDress;
+priceOfDress=900;
+System.out.println(priceOfDress);
+byte numberOfStatesInKarnataka;
+numberOfStatesInKarnataka=30;
+System.out.println(numberOfStatesInKarnataka);
+int populationOfKarnataka;
+populationOfKarnataka=60000000;
+System.out.println(populationOfKarnataka);
+long speedOfLight;
+speedOfLight=299729458;
+System.out.println(speedOfLight);
+float percentageInSslc=84;
+percentageInSslc=84;
+System.out.println(percentageInSslc);
+double constantOfElectricPermitivity;
+constantOfElectricPermitivity=8.854187e-12;
+System.out.println(constantOfElectricPermitivity);
+boolean earthIsFlat=false;
+System.out.println(earthIsFlat);
+char grade;
+grade='A';
+System.out.println(grade);
+String flowername;
+flowername="SunFlower";
+System.out.println(flowername);
+}
+}
