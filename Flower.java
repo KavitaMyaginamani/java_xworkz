a03e1936820c4cd81989ac40f8c9fb3f46f842b8
KavitaMyaginamani <72960895+KavitaMyaginamani@users.noreply.github.com>
2020-10-19 23:03:50 +0530

    sixth prgm

---
+++ Flower.java
@@ -0,0 +1,34 @@
+public class Flower
+{
+public static void main(String[] flwrs)
+{
+	//data-type var-name;
+System.out.println("Jasmin");
+short priceInWhole;
+priceInWhole=300;
+System.out.println(priceInWhole);
+byte fingersOfHand;
+fingersOfHand=10;
+System.out.println(fingersOfHand);
+int numberOfEngineeringCollegesInIndia;
+numberOfEngineeringCollegesInIndia=40000;
+System.out.println(numberOfEngineeringCollegesInIndia);
+long populationOfIndia;
+populationOfIndia=1380004385;
+System.out.println(populationOfIndia);
+float percentageOfStudent;
+percentageOfStudent=85;
+System.out.println(percentageOfStudent);
+double valueOfPi;
+valueOfPi=3.14;
+System.out.println(valueOfPi);
+boolean isPizzaTasty=true;
+System.out.println(isPizzaTasty);
+char grade;
+grade='A';
+System.out.println(grade);
+String animalname;
+animalname="Cat";
+System.out.println(animalname);
+}
+}
