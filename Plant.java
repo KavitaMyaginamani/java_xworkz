de6100968ae7dd4996373fab9ad9d4a0247d52b4
KavitaMyaginamani <72960895+KavitaMyaginamani@users.noreply.github.com>
2020-10-19 23:09:52 +0530

    ninth prgm

---
+++ Plant.java
@@ -0,0 +1,34 @@
+public class Plant
+{
+public static void main(String[] flwrs)
+{
+	//data-type var-name;
+System.out.println("Grapes Ttree");
+short priceOfBook;
+priceOfBook=250;
+System.out.println(priceOfBook);
+byte numberOfDaysInWeek;
+numberOfDaysInWeek=7;
+System.out.println(numberOfDaysInWeek);
+int priceOfCar;
+priceOfCar=800000;
+System.out.println(priceOfCar);
+long areaOfIndia;
+areaOfIndia=3287263;
+System.out.println(areaOfIndia);
+float percentageInPuc;
+percentageInPuc=85;
+System.out.println(percentageInPuc);
+double newtonConstantOfGravitation;
+newtonConstantOfGravitation=6.674e-11;
+System.out.println(newtonConstantOfGravitation);
+boolean isIamGraduate=true;
+System.out.println(isIamGraduate);
+char asciiValue;
+asciiValue='c';
+System.out.println(asciiValue);
+String name;
+name="kavita";
+System.out.println(name);
+}
+}
