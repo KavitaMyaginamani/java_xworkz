49401bbbe0c2bf837d950a46fd1588da03eb5cc8
KavitaMyaginamani <72960895+KavitaMyaginamani@users.noreply.github.com>
2020-10-20 21:11:15 +0530

    tength prgm

---
+++ Information.java
@@ -0,0 +1,23 @@
+public class Information
+{
+public static void main(String[] fg)
+{
+	System.out.println("create");
+printAdharNumber(752801002913L);
+printPhoneNumber(9724680018L);
+printEmail("abc.xworkz@gmail.com");
+
+}
+public static void printAdharNumber(final long adharNumber )
+{
+	System.out.println("adharNuber :"+adharNumber);
+}
+public static void printPhoneNumber(long phoneNumber)
+{
+	System.out.println("phoneNumber :"+phoneNumber);
+}
+public static void printEmail(String email)
+{
+	System.out.println("email :"+email);
+}
+}
