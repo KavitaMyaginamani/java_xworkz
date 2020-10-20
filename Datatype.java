e0b7569669500114730755fe2a07f56619f24c90
KavitaMyaginamani <72960895+KavitaMyaginamani@users.noreply.github.com>
2020-10-20 21:12:57 +0530

    tvelth prgm

---
+++ Datatype.java
@@ -0,0 +1,19 @@
+public class Datatype
+{
+public static void main(String[] SDG)
+{
+	
+	 printEmail("kavita1997.xworkz@gmail.com");
+	 printProgrammingLanguage("Java");
+	
+}
+public static void printEmail(String Email)
+{
+	System.out.println("Email :"+Email);
+}
+public static void printProgrammingLanguage(String ProgrammingLanguage)
+{
+	System.out.println("ProgrammingLanguage :"+ProgrammingLanguage);
+	
+}
+}
