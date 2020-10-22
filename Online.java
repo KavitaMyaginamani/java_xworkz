3e623aaade5842432aa771af97674480c8ca4604
KavitaMyaginamani <72960895+KavitaMyaginamani@users.noreply.github.com>
2020-10-22 23:20:00 +0530

    third patch1

---
+++ Online.java
@@ -0,0 +1,14 @@
+public class Online
+{
+public static void main(String[] trainees)
+{
+System.out.println("Jvm invoked main method");
+System.out.println("trainees "+trainees);
+int sizeOfMainParam=trainees.length;
+System.out.println("sizeOfMainParam "+sizeOfMainParam);
+System.out.println("elements at index 0 "+trainees[0]);
+System.out.println("elements at index 1 "+trainees[1]);
+System.out.println("exit main method");
+
+}
+}
